d5c9ce059ad31df82e513f6018a93201fb1aa0f7
Luis Diaz <ldiazn98@gmail.com>
2024-01-08 22:37:11 -0400

    Refactors and code quality

--- pphd8extract/src/bin/cli.rs
+++ pphd8extract/src/bin/cli.rs
@@ -1,25 +1,22 @@
+use std::env;
 use std::path::Path;
 use std::process::exit;
-use std::env;
 
 use rayon::prelude::*;
 
 extern crate pphd8extract;
 use pphd8extract::pphd8parser::PPHD8FileData;
 
-fn main()
-{
-    let args : Vec<String> = env::args().collect();
+fn main() {
+    let args: Vec<String> = env::args().collect();
 
-    if args.len() < 2
-    {
+    if args.len() < 2 {
         eprintln!("Error: Missing filepath argument");
         exit(1);
     }
     let filename = &args[1];
 
-    if args.len() < 3
-    {
+    if args.len() < 3 {
         eprintln!("Error: Missing output directory argument");
         exit(1);
     }
@@ -28,50 +25,57 @@ fn main()
     let pphd8_file = PPHD8FileData::parse_from_file(filename);
 
     let file = match pphd8_file {
-        Err(e) => {eprint!("{}", e.to_string()); exit(1)},
-        Ok(file) => file
+        Err(e) => {
+            eprint!("{}", e);
+            exit(1)
+        }
+        Ok(file) => file,
     };
 
     println!("Successfully extracted data from PPHD8File!");
     println!("Result:");
-    println!("{}", file.to_string());
+    println!("{}", file);
 
     println!("Extracting VAG file content...");
-    let vags = match file.get_vag_files() 
-    {
-        Err(e) => {eprint!("{}", e.to_string()); exit(1);},
-        Ok(vags) => vags
+    let vags = match file.get_vag_files() {
+        Err(e) => {
+            eprint!("{}", e);
+            exit(1);
+        }
+        Ok(vags) => vags,
     };
     println!("VAG Files successfully parsed!");
     println!("Extracting VAG files...");
 
     let output_dir_path = Path::new(output_dir);
 
-    let errors : Vec<(usize,std::io::Error)> = vags.par_iter()
+    let errors: Vec<(usize, std::io::Error)> = vags
+        .par_iter()
         .enumerate()
-        .map(|(i, vag)|{
+        .map(|(i, vag)| {
             let output_vag_filepath = output_dir_path.join(format!("extracted_{i}.vag"));
             let output_vag_filepath = output_vag_filepath.as_path();
-            println!("Extracting file {i} to {}...", output_vag_filepath.display());
-            (i, vag.write_to_file(&output_vag_filepath.to_str().unwrap().to_string()))
-    })  .filter_map(
-        |(i, output)| 
-                    match output { Err(e) => Some((i, e)), _ => None }
+            println!(
+                "Extracting file {i} to {}...",
+                output_vag_filepath.display()
+            );
+            (
+                i,
+                vag.write_to_file(&output_vag_filepath.to_str().unwrap().to_string()),
             )
+        })
+        .filter_map(|(i, output)| match output {
+            Err(e) => Some((i, e)),
+            _ => None,
+        })
         .collect();
 
-    if !errors.is_empty()
-    {
+    if !errors.is_empty() {
         eprintln!("Some files could not be extracted:");
-        for (i, error) in errors
-        {
+        for (i, error) in errors {
             eprintln!("VAG File {i} could not be extracted. Error: {error}");
         }
-    }
-    else 
-    {
+    } else {
         println!("All files successfully extracted!");
     }
-
-    return
 }
--- pphd8extract/src/pphd8parser.rs
+++ pphd8extract/src/pphd8parser.rs
@@ -1,45 +1,53 @@
+use std::fmt::Display;
 use std::fs::{self, File};
 use std::io::Write;
 use std::mem::size_of;
 use std::os::windows::fs::FileExt;
 
-/// All the data that we know how to extract from a PPHD8 file
-pub struct PPHD8FileData
-{
-    start_of_index : u32,
-    vag_entries : Vec<VAGFileEntry>,
-    n_files : usize, // still don't know if this exists
-    start_of_data : u32,
-    file : File
+/// All the data that we know how to extract from a PPHD8 file.
+///
+/// PPHD8 files have the following structure:
+/// - A **metadata section**, where it specifies other sections in the same file
+/// - An **index section**, where it lists all files contained by the PPHD8
+/// - The actual **raw data** for several VAG files
+pub struct PPHD8FileData {
+    start_of_index: u32, // Start of the index section, extracted from the word at 0x38
+    vag_entries: Vec<VAGFileEntry>, // All entries inside the file
+    n_files: usize,      // Not extracted but can be computed from the index
+    start_of_data: u32,  // Start of the data section, extracted from the word at 0xC
+    file: File,          // The file we are reading
 }
 
-/// A VAG file entry in a PPHD8 file
-struct VAGFileEntry
-{
-    frequency : u32,
-    size : u32,
+/// A VAG file entry in a PPHD8 file, as it comes from the index section.
+struct VAGFileEntry {
+    frequency: u32,
+    size: u32,
     //how far from the start of the data section in the pphd8 file is this file
-    offset_from_data_start : usize, 
+    offset_from_data_start: usize,
 }
 
-pub struct VAGFile
-{
-    frequency : u32,
-    size : u32,
-    channels : u32,
-    filename : [u8; 32],
-    body : Vec<u8>
+/// A VAG file extracted from the PPHD8 file
+pub struct VAGFile {
+    frequency: u32,
+    size: u32,
+    channels: u32,
+    filename: [u8; 32],
+    body: Vec<u8>,
 }
 
-pub enum ParseError 
-{
+/// Possible errors that could happen when parsing a VAG file
+pub enum ParseError {
     IOError(std::io::Error),
-    IncompleteVag{entry_index : usize, expected_size : u32, actual_size : u32}
+    IncompleteVag {
+        entry_index: usize,
+        expected_size: u32,
+        actual_size: u32,
+    },
 }
 
 macro_rules! read_from_file {
     ($file_variable:ident, $type_name:ident, $offset:expr) => {
-        unsafe{
+        unsafe {
             let mut buffer = [0u8; std::mem::size_of::<$type_name>()];
             let result = $file_variable.seek_read(&mut buffer, $offset)?;
             assert_eq!(result, std::mem::size_of::<$type_name>());
@@ -49,13 +57,12 @@ macro_rules! read_from_file {
     };
 }
 
-impl PPHD8FileData 
-{
-    pub fn parse_from_file(filename : &String) -> Result<PPHD8FileData, ParseError>
-    {
+impl PPHD8FileData {
+    /// Parse a VAG file from a file.
+    pub fn parse_from_file(filename: &String) -> Result<PPHD8FileData, ParseError> {
         // Try to open file:
         let file = fs::File::open(filename)?;
-        let start_of_index = read_from_file!(file, u32, 0x38) + 16*4;
+        let start_of_index = read_from_file!(file, u32, 0x38) + 16 * 4;
         let start_of_data = read_from_file!(file, u32, 0xC);
 
         // Try to parse vag file entries
@@ -63,8 +70,7 @@ impl PPHD8FileData
         let mut n_files = 0;
         let mut index_iterator = start_of_index;
 
-        while index_iterator + ((size_of::<u32>() * 3) as u32) < start_of_data
-        {
+        while index_iterator + ((size_of::<u32>() * 3) as u32) < start_of_data {
             let offset_from_data_start = read_from_file!(file, u32, index_iterator as u64) as usize;
             index_iterator += size_of::<u32>() as u32;
             let frequency = read_from_file!(file, u32, index_iterator as u64);
@@ -81,88 +87,65 @@ impl PPHD8FileData
                 continue;
             }
 
-            vag_entries.push(VAGFileEntry { frequency, size, offset_from_data_start });
+            vag_entries.push(VAGFileEntry {
+                frequency,
+                size,
+                offset_from_data_start,
+            });
             n_files += 1;
         }
 
-        Ok(
-            PPHD8FileData {
-                start_of_index,
-                start_of_data,
-                vag_entries,
-                n_files,
-                file
-            }
-        )
+        Ok(PPHD8FileData {
+            start_of_index,
+            start_of_data,
+            vag_entries,
+            n_files,
+            file,
+        })
     }
 
-    pub fn to_string(&self) -> String
-    {
-        let mut lines : Vec<String> = vec![];
-
-        lines.push(format!("start_of_index: {}\n", self.start_of_index));
-        lines.push(format!("start_of_data: {}\n", self.start_of_data));
-        lines.push(format!("n_files: {}\n", self.n_files));
-        lines.push(format!("vag_entries:\n"));
-
-        for (i,entry) in self.vag_entries.iter().enumerate()
-        {
-            lines.push(format!("\t- Entry: {i}\n"));
-            lines.push(format!("\t\t+ frequency: {}\n", entry.frequency));
-            lines.push(format!("\t\t+ size: {}\n", entry.size));
-            lines.push(format!("\t\t+ offset_from_data_start: {}\n", entry.offset_from_data_start));
-        }
-
-        lines.concat()
-    }
-
-    pub fn get_vag_files(&self) -> Result<Vec<VAGFile>, ParseError>
-    {
+    /// Get all VAG files inside this PPHD8File
+    pub fn get_vag_files(&self) -> Result<Vec<VAGFile>, ParseError> {
         let mut results = vec![];
 
-        for (i, vag_entry) in self.vag_entries.iter().enumerate()
-        {
+        for (i, vag_entry) in self.vag_entries.iter().enumerate() {
             let mut buff = vec![0u8; vag_entry.size as usize];
             let offset = vag_entry.offset_from_data_start + (self.start_of_data as usize);
 
             // Read vag file from open file.
             let result = self.file.seek_read(&mut buff, offset as u64)?;
-            if result != vag_entry.size as usize
-            {
-                return Err(
-                    ParseError::IncompleteVag { 
-                        entry_index: i, 
-                        expected_size: vag_entry.size, 
-                        actual_size: result as u32
-                    })
+            if result != vag_entry.size as usize {
+                return Err(ParseError::IncompleteVag {
+                    entry_index: i,
+                    expected_size: vag_entry.size,
+                    actual_size: result as u32,
+                });
             }
 
             let mut filename = [0u8; 32];
-            filename[0] = 'L' as u8;
-            filename[1] = 'D' as u8;
-            results.push(VAGFile{
-                frequency : vag_entry.frequency,
-                size : vag_entry.size,
-                channels : 0x00000003,
-                filename, 
-                body: buff
+            filename[0] = b'L';
+            filename[1] = b'D';
+            results.push(VAGFile {
+                frequency: vag_entry.frequency,
+                size: vag_entry.size,
+                channels: 0x00000003,
+                filename,
+                body: buff,
             });
         }
 
         Ok(results)
     }
-
 }
 
-impl VAGFile
-{
-    pub fn write_to_file(&self, filepath : &String) -> Result<(), std::io::Error>
-    {
+impl VAGFile {
+    /// Writes this VAG file to the specified file
+    pub fn write_to_file(&self, filepath: &String) -> Result<(), std::io::Error> {
         let mut new_file = fs::File::create(filepath)?;
-        let file_format_buff = ['V' as u8, 'A' as u8, 'G' as u8, 'p' as u8];
+        let file_format_buff = [b'V', b'A', b'G', b'p'];
         let mut channels_buff = get_buff_for_num(self.channels);
         channels_buff.reverse();
-        
+
         let zero_buff = [0u8; 4];
         let mut len_buff = get_buff_for_num(self.size);
         len_buff.reverse();
@@ -170,46 +153,65 @@ impl VAGFile
         freq_buff.reverse();
 
         // Actually write the file
-        new_file.write(&file_format_buff)?;
-        new_file.write(&channels_buff)?;
-        new_file.write(&zero_buff)?;
-        new_file.write(&len_buff)?;
-        new_file.write(&freq_buff)?;
-        new_file.write(&[0u8; 12])?;
-        new_file.write(&self.filename)?;
-        new_file.write(self.body.as_slice())?;
+        let _written = new_file.write(&file_format_buff)?;
+        let _written = new_file.write(&channels_buff)?;
+        let _written = new_file.write(&zero_buff)?;
+        let _written = new_file.write(&len_buff)?;
+        let _written = new_file.write(&freq_buff)?;
+        let _written = new_file.write(&[0u8; 12])?;
+        let _written = new_file.write(&self.filename)?;
+        let _written = new_file.write(self.body.as_slice())?;
 
         Ok(())
     }
 }
 
-impl ParseError
-{
-    pub fn to_string(&self) -> String
-    {
+impl Display for ParseError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            ParseError::IOError(error) => format!("Could not operate file. Error: {}", error.to_string()),
-            ParseError::IncompleteVag { 
-                entry_index, 
-                expected_size, 
-                actual_size } => 
-                    format!(
-                        "Unable to read full VAG body from file, incomplete body. VAG entry: {entry_index}. Expected size: {expected_size}, Actual size: {actual_size}")
+            ParseError::IOError(error) => write!(f, "Could not operate file. Error: {}", error)?,
+            ParseError::IncompleteVag {
+                entry_index,
+                expected_size,
+                actual_size } =>
+                    write!(f,
+                        "Unable to read full VAG body from file, incomplete body. VAG entry: {entry_index}. Expected size: {expected_size}, Actual size: {actual_size}")?
         }
+        Ok(())
     }
 }
 
-impl From<std::io::Error> for ParseError
-{
+impl Display for PPHD8FileData {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        writeln!(f, "start_of_index: {}", self.start_of_index)?;
+        writeln!(f, "start_of_data: {}", self.start_of_data)?;
+        writeln!(f, "n_files: {}", self.n_files)?;
+        writeln!(f, "vag_entries:")?;
+
+        for (i, entry) in self.vag_entries.iter().enumerate() {
+            writeln!(f, "\t- Entry: {i}")?;
+            writeln!(f, "\t\t+ frequency: {}", entry.frequency)?;
+            writeln!(f, "\t\t+ size: {}", entry.size)?;
+            writeln!(
+                f,
+                "\t\t+ offset_from_data_start: {}",
+                entry.offset_from_data_start
+            )?;
+        }
+
+        Ok(())
+    }
+}
+
+impl From<std::io::Error> for ParseError {
     fn from(value: std::io::Error) -> Self {
         ParseError::IOError(value)
     }
 }
 
-fn get_buff_for_num(num : u32) -> [u8; 4]
-{
-    unsafe 
-    {
+/// Get a buffer (byte array) from a number
+fn get_buff_for_num(num: u32) -> [u8; 4] {
+    unsafe {
         let mut buff = [0u8; std::mem::size_of::<u32>()];
         buff.as_mut_ptr().cast::<u32>().write(num);
         buff
